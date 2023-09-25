f66495dfa1aaa5cc47fc956adbe7662bc7ecd4d6
Asher Heywood Peters <339513541@GoTVDSB.ca>
2023-09-25 11:01:43 -0400

    Add project files.

---
+++ Part 5 decision structures assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_5_decision_structures_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int will, bill, phil;
+            Console.WriteLine("How old is Will?");
+            will = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How old is Bill?");
+            bill = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How old is Phil?");
+            phil = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(will + bill + phil);
+            Console.ReadLine();
+        }
+    }
+}
